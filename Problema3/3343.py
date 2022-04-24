a31997e789f4acf0c878e0927aa2009f6ee40e05
fabianoalvoliveira <104272042+fabianoalvoliveira@users.noreply.github.com>
2022-04-24 19:06:30 -0300

    Update 3343.py

--- Problema3/3343.py
+++ Problema3/3343.py
@@ -1,36 +1,31 @@
-n,x = map(int,input().split(" "))
+n,x= map(int,input().split(" "))
 k = input()
 P,M,G = map(int,input().split(" "))
 titan = list(k)
 muralha = []
-posP = list(range(len(titan)))
-posM = list(range(len(titan)))
-posG = list(range(len(titan)))
-
-for  i in range(n):
-    muralha.append(x)
-    if titan[i] == 'P': 
-       titan[i] = P
-    elif titan[i] == 'M':
-       titan[i] = M
-    if titan[i] == 'G':
-        titan[i] = G
+posP = posM = posG = 0
+muralha.append(x)
 
 cont = 0
-for  i in range(n):
-    if titan[i] == P:
-        muralha[posP[0]] = muralha[posP[0]] - titan[i]
-        if posP[0] + 1 > cont: cont = posP[0] + 1
+for  i in (range(n)):
+    if titan[i] == 'P':
+        muralha[posP] = muralha[posP] - P
+        if posP + 1 > cont: 
+            cont = posP + 1
+            muralha.append(x)
 
-    elif titan[i] == M:
-        muralha[posM[0]] = muralha[posM[0]] - titan[i]
-        if posM[0] + 1 > cont: cont = posM[0] + 1
+    elif titan[i] == 'M':
+        muralha[posM] = muralha[posM] - M
+        if posM + 1 > cont: 
+            cont = posM + 1
+            muralha.append(x)
                 
-    elif titan[i] == G:
-        muralha[posG[0]] = muralha[posG[0]] - titan[i]
-        if posG[0] + 1 > cont: cont = posG[0] + 1
-    if(muralha[posG[0]] < G): posG.pop(0)
-    if(muralha[posM[0]] < M): posM.pop(0)
-    if(muralha[posP[0]] < P): posP.pop(0)
-
+    elif titan[i] == 'G':
+        muralha[posG] = muralha[posG] - G
+        if posG + 1 > cont: 
+            cont = posG + 1
+            muralha.append(x)
+    if(muralha[posG] < G): posG += 1
+    while(muralha[posM] < M) :posM += 1
+    while(muralha[posP] < P) : posP += 1
 print(cont)
